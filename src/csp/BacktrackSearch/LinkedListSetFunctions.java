85dcb0a5739250077f52bad71ac5ad40b520af77
Tomo Bessho <tzxb018@gmail.com>
2020-03-08 16:54:46 -0500

    catch up

---
+++ src/csp/BacktrackSearch/LinkedListSetFunctions.java
@@ -0,0 +1,33 @@
+package csp.BacktrackSearch;
+
+import java.util.*;
+
+public class LinkedListSetFunctions {
+
+    // returns the unions of the two sets (expressed with a linked list)
+    public LinkedList<Integer> union(LinkedList<Integer> s1, LinkedList<Integer> s2) {
+
+        for (int j : s2) {
+            // if j is not already in s1, add to it, else ignore (no duplicates)
+            if (s1.indexOf(j) < 0) {
+                s1.add(j);
+            }
+        }
+
+        return s1;
+    }
+
+    // returns the max element of the linked list
+    public int maxInLinkedList(LinkedList<Integer> l1) {
+        int max = l1.getFirst();
+
+        for (int i = 0; i < l1.size(); i++) {
+            if (l1.get(i) > max) {
+                max = l1.get(i);
+            }
+        }
+
+        return max;
+    }
+
+}
